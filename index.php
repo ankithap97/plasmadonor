0a728d29c1e3f5ed04505ae0a3233d3ab70834aa
ankithap97 <83691827+ankithap97@users.noreply.github.com>
2021-05-07 19:19:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
   
 <?php include "templates/header.php";?>
     <ul>
-      <li>
+      
         <a href="fill.php"><h1 style="text-align:center;">Please fill the form</h></a> 
-      </li>
-      <li>
+      
+      
         <a href="get.php"><h1 style="text-align:center;">Get Information </h></a> 
-      </li>
+      
     </ul>
   <?php include "templates/footer.php"; ?>
 
