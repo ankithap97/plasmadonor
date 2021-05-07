1e3d906e0dee66b064b388bcdf510242f3001cb1
ankithap97 <83691827+ankithap97@users.noreply.github.com>
2021-05-07 17:26:04 +0530

    Update details.php

--- details.php
+++ details.php
@@ -8,7 +8,7 @@
   $tested  = $_POST['tested'];
   $date_of_testednegative  = $_POST['date_of_testednegative'];
      
-       $conn=new mysqli('127.0.0.1','root','ankitha123','test');
+       $conn=new mysqli('localhost','root','ankitha123','test');
       if($conn->connect_error)
 {
 die('connection failed :'.$conn->connect_error);
