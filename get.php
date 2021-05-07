8614cd3601c7268998c97c98a273621e7c7aa412
ankithap97 <83691827+ankithap97@users.noreply.github.com>
2021-05-07 16:50:14 +0530

    Update get.php

--- get.php
+++ get.php
@@ -1,5 +1,5 @@
 <?php
- $conn=new mysqli('localhost','root','ankitha123','test');
+ $conn=new mysqli('127.0.0.1','root','ankitha123','test');
       if($conn->connect_error)
 {
 die('connection failed :'.$conn->connect_error);
